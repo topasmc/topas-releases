2678c799cdf5fdd483ec3c0a20bfb86fda463e24
Joseph Perl <jiperl@topasmc.org>
2024-01-23 16:32:46 -0800

    update

--- topas_3_9/debian9/get_geant4_data.sh
+++ topas_3_9/debian9/get_geant4_data.sh
@@ -14,17 +14,18 @@ wget -4 https://geant4-data.web.cern.ch/geant4-data/datasets/G4PII.1.3.tar.gz
 wget -4 https://geant4-data.web.cern.ch/geant4-data/datasets/G4ENSDFSTATE.2.3.tar.gz
 wget -4 https://geant4-data.web.cern.ch/geant4-data/datasets/G4RealSurface.2.2.tar.gz
 wget -4 https://geant4-data.web.cern.ch/geant4-data/datasets/G4TENDL.1.3.2.tar.gz
-tar -zxf G4NDL.4.6.tar.gz
-tar -zxf G4EMLOW.7.13.tar.gz
-tar -zxf G4PhotonEvaporation.5.7.tar.gz
-tar -zxf G4RadioactiveDecay.5.6.tar.gz
-tar -zxf G4PARTICLEXS.3.1.1.tar.gz
-tar -zxf G4SAIDDATA.2.0.tar.gz
-tar -zxf G4ABLA.3.1.tar.gz
-tar -xzf G4INCL.1.0.tar.gz
-tar -zxf G4PII.1.3.tar.gz
-tar -zxf G4ENSDFSTATE.2.3.tar.gz
-tar -zxf G4RealSurface.2.2.tar.gz
-tar -zxf G4TENDL.1.3.2.tar.gz
+tar -zxfq G4NDL.4.6.tar.gz
+tar -zxfq G4EMLOW.7.13.tar.gz
+tar -zxfq G4PhotonEvaporation.5.7.tar.gz
+tar -zxfq G4RadioactiveDecay.5.6.tar.gz
+tar -zxfq G4PARTICLEXS.3.1.1.tar.gz
+tar -zxfq G4SAIDDATA.2.0.tar.gz
+tar -zxfq G4ABLA.3.1.tar.gz
+tar -xzfq G4INCL.1.0.tar.gz
+tar -zxfq G4PII.1.3.tar.gz
+tar -zxfq G4ENSDFSTATE.2.3.tar.gz
+tar -zxfq G4RealSurface.2.2.tar.gz
+tar -zxfq G4TENDL.1.3.2.tar.gz
 
 export TOPAS_G4_DATA_DIR=~/G4Data
+
