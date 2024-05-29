6d729ea42af56f7ff0e1fd9c96c1e2379b162756
Joseph Perl <jiperl@topasmc.org>
2024-05-28 17:27:52 -0700

    update

--- topas_3_10/debian9/get_geant4_data_and_run_topas.sh
+++ topas_3_10/debian9/get_geant4_data_and_run_topas.sh
@@ -3,6 +3,7 @@ chmod +x get_geant4_data.sh
 ./get_geant4_data.sh
 export TOPAS_G4_DATA_DIR=G4Data
 mkdir -p topasio
-cp tapasio/TsUserParameters.txt . 2> /dev/null
+cp /tmp/tapasio/*.txt . 2> /dev/null
+ls -la
 bin/topas $1
 cp -f *.* topasio/
